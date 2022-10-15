db6b0bfc321edf3bd950122ac4968d33a9727eb1
Nguyen Duc Hoang <sunlight4d@gmail.com>
2022-10-15 08:38:23 +0700

    add code

--- javaFX/javafxdemo/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.javafxdemo {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.javafxdemo to javafx.fxml;
-    exports com.example.javafxdemo;
-}
