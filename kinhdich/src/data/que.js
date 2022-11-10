0f68ddd32ccf0e5f11026243d7fc9588f4d1b305
Nguyen Duc Hoang <sunlight4d@gmail.com>
2022-11-10 13:10:10 +0700

    add code

---
+++ kinhdich/src/data/que.js
@@ -0,0 +1,426 @@
+const que8 = [
+    {
+        id: 1,
+        name: "Càn",
+        vnName: "Trời",
+        description: "Người lãnh đạo, người cha",
+        shape: "111",
+        direction: "Tây Bắc",
+        hanh: "Kim - Dương",
+        alias: "Tam liên",        
+        bodyPart: "Đầu",
+    },
+    {
+        id: 2,
+        name: "Đoài",
+        vnName: "Trạch (đầm, hồ)",
+        description: "Con gái út",
+        shape: "110",
+        direction: "Tây",        
+        hanh: "Kim - Âm",
+        alias: "Thượng khuyết",      
+        bodyPart: "Miệng",          
+    },
+    {
+        id: 3,
+        name: "Ly",
+        vnName: "Hỏa",
+        description: "Con gái thứ",
+        shape: "101",
+        direction: "Nam",        
+        hanh: "Hỏa - Âm",
+        alias: "Trung hư",      
+        bodyPart: "Mắt",  
+    },
+    {
+        id: 4,
+        name: "Chấn",
+        vnName: "Lôi-sấm sét",
+        description: "Con trai trưởng",
+        shape: "100",
+        direction: "Đông",        
+        hanh: "Mộc - Dương",
+        alias: "Chậu ngửa",      
+        bodyPart: "Chân",  
+    },
+    {
+        id: 5,
+        name: "Tốn",
+        vnName: "Phong - gió",
+        description: "Con gái trưởng",
+        shape: "011",
+        direction: "Đông nam",        
+        hanh: "Mộc - Âm",
+        alias: "Hạ đoạn",      
+        bodyPart: "Bắp đùi",  
+    },
+    {
+        id: 6,
+        name: "Khảm",
+        vnName: "Thủy - nước",
+        description: "Con trai thứ",
+        shape: "010",
+        direction: "Bắc",        
+        hanh: "Thủy - Dương",//ko có Thủy Âm
+        alias: "Trung mãn",      
+        bodyPart: "Tai",  
+    },
+    {
+        id: 7,
+        name: "Cấn",
+        vnName: "Sơn - núi",
+        description: "Con trai út",
+        shape: "001",
+        direction: "Đông bắc",        
+        hanh: "Thổ - Dương",
+        alias: "Dẫn h                                                                                              ạ",      
+        bodyPart: "Tay",  
+    },
+    {
+        id: 8,
+        name: "Khôn",
+        vnName: "Địa - đất",
+        description: "Mẹ",
+        shape: "000",
+        direction: "Tây nam",        
+        hanh: "Thổ - Âm",
+        alias: "Lục đoạn",      
+        bodyPart: "Bụng",  
+    }
+] 
+
+const que64 = [
+    {
+        id: 1,
+        name: "Thuần Càn",        
+        shape: "111-111",
+    },
+    {
+        id: 2,
+        name: "Thuần Khôn",        
+        shape: "000-000",
+    },
+    {
+        id: 3,
+        name: "Thủy Lôi Truân",        
+        shape: "100-010",
+    },
+    {
+        id: 4,
+        name: "Sơn Thủy Mông",        
+        shape: "010-001",
+    },
+    {
+        id: 5,
+        name: "Thủy Thiên Nhu",        
+        shape: "111-010",
+    },
+    {
+        id: 6,
+        name: "Thiên Thủy Tụng",        
+        shape: "010-111",
+    },
+    {
+        id: 7,
+        name: "Địa Thủy Sư",        
+        shape: "010-000",
+    },
+    {
+        id: 8,
+        name: "Thủy Địa Tỷ",        
+        shape: "000-010",
+    },
+    {
+        id: 9,
+        name: "Phong Thiên Tiểu Súc",        
+        shape: "000-010",
+    },
+    {
+        id: 10,
+        name: "Thiên Trạch Lý",        
+        shape: "110-111",
+    },
+    {
+        id: 11,
+        name: "Địa Thiên Thái",        
+        shape: "111-000",
+    },
+    {
+        id: 12,
+        name: "Thiên Địa Bĩ",        
+        shape: "111-000",
+    },
+    {
+        id: 13,
+        name: "Thiên Hỏa Đồng Nhân",        
+        shape: "101-111",
+    },
+    {
+        id: 14,
+        name: "Hỏa Thiên Đại Hữu",        
+        shape: "111-101",
+    },
+    {
+        id: 15,
+        name: "Địa Sơn Khiêm",        
+        shape: "001-000",
+    },
+    {
+        id: 16,
+        name: "Lôi Địa Dự",        
+        shape: "000-100",
+    },
+    {
+        id: 17,
+        name: "Trạch Lôi Tùy",        
+        shape: "100-110",
+    },
+    {
+        id: 18,
+        name: "Sơn Phong Cổ",        
+        shape: "011-001",
+    },
+    {
+        id: 19,
+        name: "Địa Trạch Lâm",        
+        shape: "110-000",
+    },
+    {
+        id: 20,
+        name: "Phong Địa Quan",        
+        shape: "000-011",
+    },
+    {
+        id: 21,
+        name: "Hỏa Lôi Phệ Hạp",        
+        shape: "100-101",
+    },
+    {
+        id: 22,
+        name: "Sơn Hỏa Bí",        
+        shape: "101-001",
+    },
+    {
+        id: 23,
+        name: "Sơn Địa Bác",        
+        shape: "000-001",
+    },
+    {
+        id: 24,
+        name: "Địa Lôi Phục",        
+        shape: "100-000",
+    },
+    {
+        id: 25,
+        name: "Thiên Lôi Vô Vọng",        
+        shape: "100-111",
+    },
+    {
+        id: 26,
+        name: "Sơn Thiên Đại Súc",        
+        shape: "111-001",
+    },
+    {
+        id: 27,
+        name: "Sơn Lôi Di",        
+        shape: "100-001",
+    },
+    {
+        id: 28,
+        name: "Trạch Phong Đại Quá",        
+        shape: "011-110",
+    },
+    {
+        id: 29,
+        name: "Thuần Khảm",        
+        shape: "010-010",
+    },
+    {
+        id: 30,
+        name: "Thuần Ly",        
+        shape: "101-101",
+    },
+    {
+        id: 31,
+        name: "Trạch Sơn Hàm",        
+        shape: "001-110",
+    },
+    {
+        id: 32,
+        name: "Lôi Phong Hằng",        
+        shape: "011-100",
+    },
+    {
+        id: 33,
+        name: "Thiên Sơn Độn",        
+        shape: "001-111",
+    },
+    {
+        id: 34,
+        name: "Lôi Thiên Đại Tráng",        
+        shape: "111-100",
+    },
+    {
+        id: 35,
+        name: "Hỏa Địa Tấn",        
+        shape: "000-101",
+    },
+    {
+        id: 36,
+        name: "Địa Hỏa Minh Di",        
+        shape: "101-000",
+    },
+    {
+        id: 37,
+        name: "Phong Hỏa Gia Nhân",        
+        shape: "101-011",
+    },
+    {
+        id: 38,
+        name: "Hỏa Trạch Khuê",        
+        shape: "110-101",
+    },
+    {
+        id: 39,
+        name: "Thủy Sơn Kiển",        
+        shape: "001-010",
+    },
+    {
+        id: 40,
+        name: "Lôi Thủy Giải",        
+        shape: "010-100",
+    },
+    {
+        id: 41,
+        name: "Sơn Trạch Tổn",        
+        shape: "110-001",
+    },
+    {
+        id: 42,
+        name: "Phong Lôi Ích",        
+        shape: "100-011",
+    },
+    {
+        id: 43,
+        name: "Trạch Thiên Quải",        
+        shape: "111-110",
+    },
+    {
+        id: 44,
+        name: "Thiên Phong Cấu",        
+        shape: "011-111",
+    },
+    {
+        id: 45,
+        name: "Trạch Địa Tụy",        
+        shape: "000-110",
+    },
+    {
+        id: 46,
+        name: "Địa Phong Thăng",        
+        shape: "011-000",
+    },
+    {
+        id: 47,
+        name: "Trạch Thủy Khốn",        
+        shape: "010-110",
+    },
+    {
+        id: 48,
+        name: "Thủy Phong Tỉnh",        
+        shape: "011-010",
+    },
+    {
+        id: 49,
+        name: "Trạch Hỏa Cách",        
+        shape: "101-110",
+    },
+    {
+        id: 50,
+        name: "Hỏa Phong Đỉnh",        
+        shape: "011-101",
+    },
+    {
+        id: 51,
+        name: "Thuần Chấn",        
+        shape: "100-100",
+    },
+    {
+        id: 52,
+        name: "Thuần Cấn",        
+        shape: "001-001",
+    },
+    {
+        id: 53,
+        name: "Phong Sơn Tiệm",        
+        shape: "001-011",
+    },
+    {
+        id: 54,
+        name: "Lôi Trạch Quy Muội",        
+        shape: "110-100",
+    },
+    {
+        id: 55,
+        name: "Lôi Hỏa Phong",        
+        shape: "101-100",
+    },
+    {
+        id: 56,
+        name: "Hỏa Sơn Lữ",        
+        shape: "001-101",
+    },
+    {
+        id: 57,
+        name: "Thuần Tốn",        
+        shape: "011-011",
+    },
+    {
+        id: 58,
+        name: "Thuần Đoài",        
+        shape: "110-110",
+    },
+    {
+        id: 59,
+        name: "Phong Thủy Hoán",        
+        shape: "010-011",
+    },
+    {
+        id: 60,
+        name: "Thủy Trạch Tiết",        
+        shape: "110-010",
+    },
+    {
+        id: 61,
+        name: "Phong Trạch Trung Phu",        
+        shape: "110-011",
+    },
+    {
+        id: 62,
+        name: "Lôi Sơn Tiểu Quá",        
+        shape: "001-100",
+    },
+    {
+        id: 63,
+        name: "Thủy Hỏa Ký Tế",        
+        shape: "101-010",
+    },
+    {
+        id: 64,
+        name: "Hỏa Thủy Vị Tế",        
+        shape: "010-101",
+    }
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
